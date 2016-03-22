478b1c968c3a4e3221b8aacc34667b84dd8604dc
Kamil Wojciech <kamil.wojciech@schibsted.pl>
2016-03-22 08:43:37 +0100

    update version

--- git-msg-prefix.rb
+++ git-msg-prefix.rb
@@ -1,8 +1,8 @@
 class GitMsgPrefix < Formula
   desc ""
   homepage "https://github.com/KamilWojciech/git-msg-prefix"
-  url "https://github.com/KamilWojciech/git-msg-prefix/archive/1.0.2.tar.gz"
-  version "1.0.2"
+  url "https://github.com/KamilWojciech/git-msg-prefix/archive/1.0.3.tar.gz"
+  version "1.0.3"
   sha256 "e4f93ef5595df97f7a1c8659e35ec2db2bb2f3bb8ff80ce8c1d8b9ed9e1f52c6"
 
   def install
