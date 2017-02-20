9ceb261d13182795d274ee2b1e00d9de48b256ef
Travis CI <travis>
2017-02-20 17:14:22 +0000

    Update formula git-msg-prefix to 1.0.7-dev5

--- git-msg-prefix.rb
+++ git-msg-prefix.rb
@@ -1,9 +1,9 @@
 class GitMsgPrefix < Formula
   desc ""
   homepage "https://github.com/KamilWojciech/git-msg-prefix"
-  url "https://github.com/KamilWojciech/git-msg-prefix/releases/download/1.0.7-dev4/1.0.7-dev4.tar.gz"
-  version "1.0.7-dev4"
-  sha256 "ecd9639670ef5e65a96a2e46c3569fef1fdcd5835d16602da049da0089374baa"
+  url "https://github.com/KamilWojciech/git-msg-prefix/releases/download/1.0.7-dev5/1.0.7-dev5.tar.gz"
+  version "1.0.7-dev5"
+  sha256 "974310313868b31fffe426769e97ed1e1dfcc5c0200368f5f9df834ec716d054"
 
   def install
     bin.install "bin/git-msg-prefix"
