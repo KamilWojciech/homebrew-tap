be61bac7993f09862a7ec359072426808b0db1be
Travis CI <travis>
2017-02-21 07:48:57 +0000

    Update formula git-msg-prefix to 1.1.4

--- git-msg-prefix.rb
+++ git-msg-prefix.rb
@@ -1,17 +1,18 @@
 class GitMsgPrefix < Formula
   desc ""
   homepage "https://github.com/KamilWojciech/git-msg-prefix"
-  url "https://github.com/KamilWojciech/git-msg-prefix/releases/download/1.1.3/git-msg-prefix-1.1.3.tar.gz"
-  version "1.1.3"
-  sha256 "23918611f49077bf09b5c81221c178db12de3e0f6fa10856a681ef39bce44228"
+  url "https://github.com/KamilWojciech/git-msg-prefix/releases/download/1.1.4/git-msg-prefix-1.1.4.tar.gz"
+  version "1.1.4"
+  sha256 "c603fd97713decd3c5948f5ae573a94d409c2318e71037486796737f6604c590"
 
   def install
     bin.install "bin/git-msg-prefix"
     lib.install Dir["lib/*"]
     FileUtils.chmod 0555, lib + 'git-hook/prepare-commit-msg'
     homeDir = File.expand_path("~" + ENV['USER'])
-    gitHookFile = homeDir + '/.git-templates/hooks/prepare-commit-msg'
-    FileUtils.mkdir_p(gitHookFile) unless File.exists?(gitHookFile)
+    gitHookDir = homeDir + '/.git-templates/hooks/'
+    gitHookFile = gitHookDir + 'prepare-commit-msg'
+    FileUtils.mkdir_p(gitHookDir) unless File.exists?(gitHookDir)
 
     linked = false
     if File.symlink?(gitHookFile)
