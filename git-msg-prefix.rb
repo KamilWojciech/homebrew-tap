1dca57f898fb559419ac901181d11a3efcbdec5b
Travis CI <travis>
2017-02-22 08:41:12 +0000

    Update formula git-msg-prefix to 1.2.4

--- git-msg-prefix.rb
+++ git-msg-prefix.rb
@@ -1,9 +1,9 @@
 class GitMsgPrefix < Formula
   desc ""
   homepage "https://github.com/KamilWojciech/git-msg-prefix"
-  url "https://github.com/KamilWojciech/git-msg-prefix/releases/download/1.2.3/git-msg-prefix-1.2.3.tar.gz"
-  version "1.2.3"
-  sha256 "a89e82814c76906f98f82592adfb203feac05bf3817a74150cbc9b14e983ec5d"
+  url "https://github.com/KamilWojciech/git-msg-prefix/releases/download/1.2.4/git-msg-prefix-1.2.4.tar.gz"
+  version "1.2.4"
+  sha256 "4aad05a3fe54b8abff5262e9b93062d48a00d12230533b6a7bafa60739a37192"
 
   def install
     bin.install "bin/git-msg-prefix"
@@ -14,26 +14,30 @@ class GitMsgPrefix < Formula
     gitHookFile = gitHookDir + 'prepare-commit-msg'
     FileUtils.mkdir_p(gitHookDir) unless File.exists?(gitHookDir)
 
+    libLinkFile = "/usr/local/lib/prepare-commit-msg"
+
     linked = false
     if File.symlink?(gitHookFile)
       symlinkTarget = File.readlink(gitHookFile)
-      if File.fnmatch File.expand_path(prefix + '../') + '**prepare-commit-msg', symlinkTarget
+      if File.fnmatch libLinkFile, symlinkTarget
         linked = true
       end
     end
+
+    lib.install_symlink lib + 'git-hook/prepare-commit-msg'
 
     if File.exist?(gitHookFile) && !linked
       print "File '" + gitHookFile + "' already exists! Do you want to replace it? [Y/n]: "
       input = STDIN.gets.strip
       if input.downcase != 'y'
         return false
+      else
+        File.delete gitHookFile
       end
     end
 
-    libLinkFile = "/usr/local/lib/prepare-commit-msg"
-    File.delete gitHookFile unless !File.exists?(gitHookFile)
-    lib.install_symlink lib + 'git-hook/prepare-commit-msg'
-    File.delete libLinkFile unless !File.exists?(libLinkFile)
-    FileUtils.ln_s libLinkFile, gitHookFile
+    if !linked
+      FileUtils.ln_s libLinkFile, gitHookFile
+    end
   end
 end
